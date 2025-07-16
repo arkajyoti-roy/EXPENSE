0247b8449d406c11e60854e9ca4fd692ac87efe8
Arkajyoti Roy <163657852+arkajyoti-roy@users.noreply.github.com>
2025-07-16 18:26:51 +0530

    update AllComp.jsx

--- src/AllComp.jsx
+++ src/AllComp.jsx
@@ -1,30 +1,22 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
 import OverviewCards from "./components/OverviewCards/OverviewCards.jsx";
 import ActionButton from "./components/ActionButtons/ActionButton.jsx";
-// import RecentTransactions from "./components/RecentTransactions/RecentTransactions.jsx";
-// import BudgetOverview from './components/BudgetOverview/BudgetOverview.jsx';
-// import RecurringRules from './components/RecurringRules/RecurringRules.jsx';
-// import BudgetRecurring from "./components/BudgetRecurring.jsx";
 import RecentTranBudRecuu from "./components/RecentTranBudRecuu.jsx";
 import DashboardWrapper from "./components/Budgetoverview/DashboardWrapper.jsx";
 function AllComp() {
   return (
     <>
-    <div className="-mt-1">
-      <Navbar />
+      <div className="-mt-1">
+        <Navbar />
       </div>
       <DashboardWrapper>
-      <div className="pt-20">
-        <OverviewCards />
+        <div className="pt-20">
+          <OverviewCards />
 
-        <ActionButton />
-        <RecentTranBudRecuu />
-      </div>
+          <ActionButton />
+          <RecentTranBudRecuu />
+        </div>
       </DashboardWrapper>
-      {/* <RecentTransactions /> */}
-      {/* <BudgetOverview /> */}
-      {/* <RecurringRules /> */}
-      {/* <BudgetRecurring /> */}
       <footer className="text-center text-gray-500 text-xs p-4">
         &copy; {new Date().getFullYear()} Expense Tracker. All rights reserved.
       </footer>
